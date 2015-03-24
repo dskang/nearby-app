eefa6c26bfa3922bcfddc974423fa89a6e63e231
Dan Kang <dan@dskang.com>
2015-03-24 11:07:26 -0400

    Add Parse log in and sign up view controllers

--- Nearby/ViewController.swift
+++ Nearby/ViewController.swift
@@ -9,22 +9,34 @@
 import UIKit
 import Parse
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, PFLogInViewControllerDelegate, PFSignUpViewControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        var user = PFUser()
-        user.username = "DK"
-        user.password = "password"
-        user.email = "dan@dskang.com"
+    }
 
-        user.signUpInBackgroundWithBlock() { (succeeded, error) in
-            if error == nil {
-            } else {
-                let errorString = error.userInfo!["error"] as String
-                println(errorString)
-            }
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+
+        if PFUser.currentUser() == nil {
+            // Create the log in view controller
+            let logInController = PFLogInViewController()
+            logInController.delegate = self
+            logInController.fields = (PFLogInFields.UsernameAndPassword
+                | PFLogInFields.LogInButton
+                | PFLogInFields.SignUpButton
+                | PFLogInFields.PasswordForgotten)
+
+            // Create the sign up view controller
+            let signUpController = PFSignUpViewController()
+            signUpController.delegate = self
+
+            // Assign our sign up controller to be displayed from our login controller
+            logInController.signUpController = signUpController
+
+            // Present the log in view controller
+            self.presentViewController(logInController, animated: true, completion: nil)
         }
     }
 
@@ -33,6 +45,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func logInViewController(logInController: PFLogInViewController!, didLogInUser user: PFUser!) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    func signUpViewController(signUpController: PFSignUpViewController!, didSignUpUser user: PFUser!) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
 
 }
 
